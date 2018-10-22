a7f3774d0c03ae6952efc3a73439fa8b26df41e4
Ecopaynet Development <idi@ecopaynet.com>
2018-10-22 13:26:51 +0200

    EcoA10 Library v1.15

---
+++ docs/enumcom_1_1ecopaynet_1_1ecoa10_1_1_extra_parameters_1_1_refund_type.js
@@ -0,0 +1,5 @@
+var enumcom_1_1ecopaynet_1_1ecoa10_1_1_extra_parameters_1_1_refund_type =
+[
+    [ "getValue", "enumcom_1_1ecopaynet_1_1ecoa10_1_1_extra_parameters_1_1_refund_type.html#ad009a77f0f96086d8ba7390bda8af951", null ],
+    [ "STANDARD", "enumcom_1_1ecopaynet_1_1ecoa10_1_1_extra_parameters_1_1_refund_type.html#a1c950ca1d01fee426bde47cb721ed62e", null ]
+];
